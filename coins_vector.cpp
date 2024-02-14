c9a78e1f29197101283b9051df7d1f275848473d
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-02-14 21:28:10 +0100

    Create coins_vector.cpp

---
+++ coins_vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> makeChange(int amount, const vector<int>& coins) {
+    vector<int> result;
+  
+    vector<int> sortedCoins(coins);
+    sort(sortedCoins.rbegin(), sortedCoins.rend());
+
+    for (int i = 0; i < coins.size(); ++i) {
+        while (amount >= sortedCoins[i]) {
+            // Add the largest coin that is less than or equal to the remaining amount
+            result.push_back(sortedCoins[i]);
+            amount -= sortedCoins[i];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int amount = 67;
+    vector<int> coins = {25, 10, 5, 1};
+    
+    vector<int> change = makeChange(amount, coins);
+
+    cout << "Coins used to make change for " << amount << " cents:" << endl;
+    for (int coin : change) {
+        cout << coin << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
